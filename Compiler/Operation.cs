62f82ca231a3de9908a16a0383540940ac139bf4
Marcelo Bonow <marcelobonowott@hotmail.com>
2019-01-12 15:07:52 -0200

    Adicionado operações
    Adiconado a base das operações basicas da aritimética

--- Compiler/Operation.cs
+++ Compiler/Operation.cs
@@ -13,4 +13,34 @@ namespace Compiler
             return number1 + number2;
         }
     }
+    class Sum : Operation
+    {
+        public override float DoOperation(float number1, float number2)
+        {
+            return number1 + number2;
+        }
+    }
+    class Subtraction : Operation
+    {
+        public override float DoOperation(float number1, float number2)
+        {
+            return number1 - number2;
+        }
+    }
+    class Multiply : Operation
+    {
+        public override float DoOperation(float number1, float number2)
+        {
+            return number1 * number2;
+        }
+    }
+    class Divide : Operation
+    {
+        public override float DoOperation(float number1, float number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
+
 }
